3985c1bb40963cdb5f4990e4ee100ec02c83aed3
SpiderMan <hrushikesh.tk@gmail.com>
2026-02-13 17:27:55 +0530

    setting up user-auth-service

---
+++ src/test/java/com/hrushikesh/user_auth_service_v2/UserAuthServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrushikesh.user_auth_service_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
